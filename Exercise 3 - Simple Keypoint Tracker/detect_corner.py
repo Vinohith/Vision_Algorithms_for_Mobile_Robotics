ef8b3b951c4fb2f742b8532c769c4ed68953bab6
Vinohith <vinohith1998@gmail.com>
2020-08-02 22:52:51 +0530

    Add harris keypoint detector

--- Exercise 3 - Simple Keypoint Tracker/detect_corner.py
+++ Exercise 3 - Simple Keypoint Tracker/detect_corner.py
@@ -12,11 +12,9 @@ def harris(img, patch_size, kappa):
     sobel_y = sobel_x.T 
     Ix = sig.convolve2d(img, sobel_x, mode='same')
     Iy = sig.convolve2d(img, sobel_y, mode='same')
-    print(Ix.shape, Iy.shape)
     Ixx = Ix ** 2
     Iyy = Iy ** 2
     IxIy = Ix * Iy
-    print(Ixx.shape, Iyy.shape, IxIy.shape)
     # rectangular unifoem window
     patch_weights = np.ones((patch_size, patch_size))/patch_size**2
     sumIxx = sig.convolve2d(Ixx, patch_weights, mode='same')
@@ -26,9 +24,23 @@ def harris(img, patch_size, kappa):
     traceM = sumIxx * sumIyy
     scores = detM - (kappa * traceM**2)
     scores[scores < 0] = 0
-    print(scores.shape)
     return scores
 
+
+def ind2sub(array_shape, ind):
+    rows = ind // array_shape[1]
+    cols = ind % array_shape[1]
+    return (rows, cols)
+
+
+def select_keypoints(scores, num, r):
+    keypoints = np.ones((2, num))
+    for i in range(num):
+        [row, col] = ind2sub(scores.shape, np.argmax(scores))
+        keypoints[:, i] = [row, col]
+        scores[max(0, row-r):min(scores.shape[0],row+r+1), max(0, col-r):min(scores.shape[1], col+r+1)] = \
+            np.zeros((min(scores.shape[0],row+r+1)-max(0, row-r), min(scores.shape[1], col+r+1)-max(0, col-r)))
+    return keypoints
 
 
 corner_patch_size = 9
@@ -43,6 +55,9 @@ print(img.shape)
 
 # Corner Response (Harris)
 harris_score = harris(img, corner_patch_size, harris_kappa)
+keypoints = select_keypoints(harris_score, num_keypoints, nonmaximum_supression_radius)
 
-plt.imshow(harris_score, cmap=plt.get_cmap('gray'))
+plt.figure(figsize=(15,5))
+plt.imshow(img, cmap=plt.get_cmap('gray'))
+plt.scatter(keypoints[1], keypoints[0], marker='x', color='r')
 plt.show()
